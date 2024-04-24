60587eb8668939e76e782cd255fda1fabcdc5a42
micem2 <freakishmice@proton.me>
2024-04-24 00:44:48 -0700

    created post routes

---
+++ api/src/routes/postRoutes.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { addPost, deletePost, editPost, getAllPost, getOnePost } from "../controllers/postController";
+
+const router = Router();
+
+router.get('/', getAllPost);
+router.get('/:id', getOnePost);
+router.post('/', addPost);
+router.put(':id', editPost);
+router.delete('/:id', deletePost);
+
+export default router;

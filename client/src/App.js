6b9323071268a58c390c15851e77bde40001ac9c
micem2 <freakishmice@proton.me>
2024-04-23 21:47:24 -0700

    added registeration page

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { SiteNavbar } from './components/Navbar';
+import { UserProvider } from './contexts/UserContext';
+import { Register } from './components/Register';
 
 function App() {
   return (
-    <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<SiteNavbar />}>
-
-      </Route>
-    </Routes>
-    </BrowserRouter>
+    <UserProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<SiteNavbar />}>
+            <Route path='register' element={<Register />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </UserProvider>
   );
 }
 

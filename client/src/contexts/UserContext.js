9188a9badea5c99dbd79e72f3a137f00b1753720
micem2 <freakishmice@proton.me>
2024-04-23 18:20:55 -0700

    renamed AuthContext to UserContext

---
+++ client/src/contexts/UserContext.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+import React, { createContext } from "react";
+
+export const UserContext = createContext();
+
+export const UserProvider = (props) => {
+    const apiBaseUrl = "http://localhost:3000/api/users";
+
+    function registerUser(user, pass) {
+        let credentials = { user, pass };
+
+        return axios.post(apiBaseUrl, credentials)
+        .then(response => {
+            return new Promise(resolve => resolve(response.data));
+        });
+    };
+
+    function loginUser(user, pass) {
+        let credentials = { user, pass };
+
+        return axios.post(baseUrl + '/login', credentials)
+        .then(response => {
+            localStorage.setItem('authToken', response.data.token)
+            return new Promise(resolve => resolve(response.data));
+        });
+    };
+
+    return (
+        <UserContext.Provider value={{ registerUser, loginUser }}>
+            { props.children }
+        </UserContext.Provider>
+    )
+};

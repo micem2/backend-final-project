1045a9e46b99aa6f0ca3ad194c6509c7d653d370
micem2 <freakishmice@proton.me>
2024-04-26 00:11:57 -0700

    account page gets information by user id

--- client/src/components/Account.js
+++ client/src/components/Account.js
@@ -1,1 +1,44 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
+import { Form, Button } from "react-bootstrap";
+import { PostContext } from "../contexts/PostContext";
+import { useNavigate, useParams } from "react-router-dom";
+import { UserContext } from "../contexts/UserContext";
+
+export const Account = () => {
+    let params = useParams();
+    const [user, setUser] = useState({
+        id: params.Id,
+        username: "",
+        country: "",
+        gender: ""
+    });
+
+    let { getUser } = useContext(UserContext);
+    let navigate = useNavigate();
+    let { id, username, country, gender } = user
+
+    useEffect(() => {
+        if (id === undefined) return
+        async function fetch() {
+            await getUser(id).then((user) => setUser(user))
+        }
+        fetch();
+    })
+
+    return (
+        <>
+        <h2>Account Profile for {username}</h2>
+        <Form className="align-self-start">
+            <Form.Group className="mb-3">
+                <Form.Label>Country</Form.Label>
+                <Form.Control type="text" name="country" value={country} />
+            </Form.Group>
+            <Form.Group className="mb-3">
+                <Form.Label>Gender</Form.Label>
+                <Form.Control type="text" name="gender" value={gender} />
+            </Form.Group>
+            <Button type="submit" variant="success">Save</Button>
+        </Form>
+        </>
+    );
+};

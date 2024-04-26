75e398ead74cd72f684334ed1100ad27c8087fc9
micem2 <freakishmice@proton.me>
2024-04-25 23:11:21 -0700

    added route to find user id by token

--- api/src/routes/userRoutes.ts
+++ api/src/routes/userRoutes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import { createUser, loginUser, returnUser } from '../controllers/userController';
+import { createUser, loginUser, returnUser, returnUserId } from '../controllers/userController';
 
 const router = Router();
 
 router.post('/', createUser);
 router.post('/login', loginUser);
 router.post('/finduser', returnUser);
+router.post('/findid', returnUserId);
 
 export default router;

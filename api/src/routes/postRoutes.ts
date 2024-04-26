385a4e632dfe401570592eed6ade19091f1ed312
micem2 <freakishmice@proton.me>
2024-04-26 14:00:21 -0700

    fixed a small typo

--- api/src/routes/postRoutes.ts
+++ api/src/routes/postRoutes.ts
@@ -6,7 +6,7 @@ const router = Router();
 router.get('/', getAllPost);
 router.get('/:id', getOnePost);
 router.post('/', addPost);
-router.put(':id', editPost);
+router.put('/:id', editPost);
 router.delete('/:id', deletePost);
 
 export default router;
